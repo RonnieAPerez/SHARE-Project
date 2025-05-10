2ffadff6ad591858bd11d3b571efa4fcdb9a27e3
Kingcodes95 <Ronniecodes95@gmail.com>
2025-05-10 15:34:24 -0500

    Implemented Create volunteer for admins on the dashboard

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,91 @@
-import './Dashboard.css';
+import "./Dashboard.css";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Dashboard() {
-    return(
-        <div>
-            <p>Admin DashBoard</p>
-        </div>
-    )
+	const navigate = useNavigate();
+	const [showCreate, setShowCreate] = useState(false);
+
+	const [error, setError] = useState("");
+	const [success, setSuccess] = useState("");
+
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const role = "volunteer";
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setError("");
+		setSuccess("");
+
+		try {
+			const res = await fetch("http://localhost:8000/auth/signup", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ username, password, role }),
+			});
+
+			const data = await res.json();
+			if (!res.ok) {
+				throw new Error(data.detail || "Login failed");
+			}
+
+			setSuccess("Volunteer successfully created!");
+			setUsername("");
+			setPassword("");
+
+		} catch (err) {
+			setError(err.message);
+		}
+	};
+
+	return (
+		<div className="Dashboard-Page">
+			<div className="Title-Dashboard">
+				<div className="Home-Title-Dashboard">
+					<button onClick={() => navigate("/data")}>Home</button>
+					<p>Admin Dashboard</p>
+				</div>
+				<hr />
+				<div className="Buttons-Dashboard">
+					<button
+						className="Create-Volunteer-Button"
+						onClick={() => setShowCreate((prev) => !prev)}>
+						Create Volunteer
+					</button>
+				</div>
+
+				{showCreate && (
+					<div className="Create-Volunteer-Open">
+						<h3>Create a new volunteer</h3>
+						<form onSubmit={handleSubmit}>
+							<input
+								type="text"
+								placeholder="Username"
+								value={username}
+								onChange={(e) => setUsername(e.target.value)}
+								required
+							/>
+							<input
+								type="password"
+								placeholder="Password"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								required
+							/>
+							<button className="Volunteer-Submit" type="submit">
+								Submit
+							</button>
+						</form>
+							{error && <p className="error-message">{error}</p>}
+							{success && <p className="success-message">{success}</p>}
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default Dashboard;
--- frontend/src/pages/PeoplesData.jsx
+++ frontend/src/pages/PeoplesData.jsx
@@ -1,20 +1,38 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import SearchData from "../components/SearchData/SearchData";
 import DataCenter from "../components/DataCenter/DataCenter";
 import LogoutButton from "../components/LogoutButton/LogoutButton";
 import Add_Client_Button from "../components/Add_Client_Button/Add_Client_Button";
 
 function PeoplesData() {
-  const [searchQuery, setSearchQuery] = useState("");
 
-  return (
-    <div className="Peoples-Data-page">
-      <SearchData searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-      <DataCenter searchQuery={searchQuery} />
-      <Add_Client_Button />
-      <LogoutButton />
-    </div>
-  );
+  const navigate = useNavigate();
+	const rawUser = localStorage.getItem("user");
+	const loggedInUser = rawUser ? JSON.parse(rawUser) : {};
+	const role = loggedInUser.role;
+
+	const [searchQuery, setSearchQuery] = useState("");
+
+	return (
+		<div className="Peoples-Data-page">
+			<SearchData searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+			<DataCenter searchQuery={searchQuery} />
+			<div className="Data-Buttons">
+				<div className="Add-Client-Button">
+					<Add_Client_Button />
+				</div>
+				{(role === "admin" || role === "super_admin") && (
+					<div>
+						<button onClick={() => navigate("/dashboard")}>Admin Dashboard</button>
+					</div>
+				)}
+				<div className="Logout-Button">
+					<LogoutButton />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default PeoplesData;
